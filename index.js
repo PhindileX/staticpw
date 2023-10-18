c29b56a2bf9caa5ccb379c552b1501c8374f8311
Phindile <xphindile5@gmail.com>
2023-10-18 22:07:50 +0200

    layout for static init commit

---
+++ index.js
@@ -0,0 +1,9 @@
+window.onload=function(){
+    let about =document.querySelector("#about")
+    
+    about.addEventListener("click", function() {
+       location.replace("./about_.html")
+    });
+
+  }
+console.log("Hi Script")
