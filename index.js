5fd6cbda76740c58eab27f5b2d9fcf62e4a6dcff
PhindileX <39278692+PhindileX@users.noreply.github.com>
2023-11-14 22:57:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-window.onload=function(){
-    let about =document.querySelector("#about")
-    
-    about.addEventListener("click", function() {
-       location.replace("./about_.html")
-    });
 
-  }
-console.log("Hi Script")
